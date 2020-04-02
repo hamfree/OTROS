a9b7cef6656187d53bebe55875eb0d824418740c
Juan F. Ruiz Fernandez <juanfco.ruiz@gmail.com>
2020-04-02 17:12:43 +0200

    Adapto el script 'setjdk.cmd' a mi nuevo entorno de desarrollo

--- Scripts/FILEMON/setjdk.cmd
+++ Scripts/FILEMON/setjdk.cmd
@@ -14,17 +14,16 @@
 @rem *    la ruta "C:\des\bin". Las aplicaciones cuyas rutas se agregan al path         *
 @rem *    son:                                                                          *
 @rem *    - OpenSSL                                                                     *
-@rem *    - Servidor NodeJS                                                             *
 @rem *    - Git                                                                         *
-@rem *    - npm (gestor de paquetes de NodeJs)                                          *
 @rem *    - Lenguaje de programacion 'Python'                                           *
-@rem *    - Lenguaje de programacion 'Perl'                                             *
-@rem *    - Maven                                                                       *
-@rem *    - Gradle                                                                      *
-@rem *    - Servidor J2EE Glassfish                                                     *
-@rem *    - Servidor J2EE Payara                                                        *
+@rem *    - Servidor J2EE Tomee                                                         *
 @rem *    - Contenedor de servlets/JSP Tomcat                                           *
-@rem *    - Lenguaje de programacion Java (versiones 1.8, 11 y 13)                      *
+@rem *    - Lenguaje de programacion Java (versiones 1.8, 11)                           *
+@rem *                                                                                  *
+@rem *    Historia:                                                                     *
+@rem *    - 2020/04/02 - Se agregan las rutas del software de Intel y Nvidia.           *
+@rem *      Se cambian las rutas de desarrollo.                                         *
+@rem *                                                                                  *
 @rem ************************************************************************************
 
 @echo off
@@ -49,31 +48,36 @@ set PATHSYS=%PATHSYS%;%POWERSHELL%
 @rem El resto de rutas de otras aplicaciones incluidas en el path del sistema
 set PATHSYS=%PATHSYS%;C:\Program Files\PuTTY
 set PATHSYS=%PATHSYS%;C:\Program Files\Calibre2
-set PATHSYS=%PATHSYS%;C:\Program Files ^(x86^)\GnuPG\bin
+set PATHSYS=%PATHSYS%;C:\Program Files ^(x86^)\gnupg\bin
 set PATHSYS=%PATHSYS%;C:\Program Files ^(x86^)\ZeroTier\One
 set PATHSYS=%PATHSYS%;C:\Program Files\MySQL\MySQL Server 8.0\bin
+set PATHSYS=%PATHSYS%;C:\Program Files\Intel\Intel^(R^) Management Engine Components\DAL
+set PATHSYS=%PATHSYS%;C:\Program Files\Intel\Intel^(R^) Management Engine Components\IPT
+set PATHSYS=%PATHSYS%;C:\Program Files\Intel\iCLS Client\
+set PATHSYS=%PATHSYS%;C:\Program Files\Intel\WiFi\bin\
+set PATHSYS=%PATHSYS%;C:\Program Files\NVIDIA Corporation\NVIDIA NvDLISR
+set PATHSYS=%PATHSYS%;C:\Program Files\Common Files\Intel\WirelessCommon\
+set PATHSYS=%PATHSYS%;C:\Program Files ^(x86^)\Intel\Intel^(R^) Management Engine Components\IPT
+set PATHSYS=%PATHSYS%;C:\Program Files ^(x86^)\Intel\Intel^(R^) Management Engine Components\DAL
+set PATHSYS=%PATHSYS%;C:\Program Files ^(x86^)\Intel\iCLS Client\
+set PATHSYS=%PATHSYS%;C:\Program Files ^(x86^)\NVIDIA Corporation\PhysX\Common
+
 
 @rem Variables para herramientas del entorno de desarrollo
 set PATHDES=C:\des\bin
 
-@rem Variable necesaria para la ejecución de Tomcat
+@rem Variable necesaria para la ejecuciÃ³n de Tomcat
 set CATALINA_HOME=C:\des\bin\tomcat
 
 @rem Aqui van las rutas a los binarios de las herramientas de desarrollo.
 set OPENSSL=%PATHDES%\openssl\bin
-set NODE=%PATHDES%\nodejs
 set GIT=%PATHDES%\git\bin
-set NPM=%USERPROFILE%\AppData\Roaming\npm
-set PYTHON=%PATHDES%\python;%PATHDES%\python\Scripts;C:\Users\hamfree\AppData\Roaming\Python\Python38\Scripts
-set PERL=%PATHDES%\perl\site\bin;%PATHDES%\perl\bin
-set MVN_HOME=%PATHDES%\maven
-set GRADLE_HOME=%PATHDES%\gradle
-set GLASSFISH=%PATHDES%\glassfish\bin;%PATHDES%\glassfish\glassfish\bin
+set PYTHON=%PATHDES%\python;%PATHDES%\python\Scripts
 set TOMCAT=%PATHDES%\tomcat\bin
-set PAYARA=%PATHDES%\payara\bin;%PATHDES%\payara\glassfish\bin
+set TOMEE=%PATHDES%\tomee\bin
 
 @rem Componemos el path de las herramientas de desarrollo.
-set PATHDES=%PATHDES%;%MVN_HOME%\bin;%GRADLE_HOME%\bin;%GLASSFISH%;%TOMCAT%;%PAYARA%;%PYTHON%;%PERL%;%NODE%;%NPM%;%GIT%
+set PATHDES=%PATHDES%;%TOMCAT%;%TOMEE%;%PYTHON%;%GIT%
 
 @rem Eliminamos el contenido actual de JAVA_HOME
 set JAVA_HOME=
@@ -88,7 +92,6 @@ echo.
 echo.
 echo      1. JDK 1.8
 echo      2. JDK 11
-echo      3. JDK 13
 echo      0. SALIR
 echo.
 echo.
@@ -96,14 +99,13 @@ echo.
 set /p var=
 if %var%==1 goto :jdk8
 if %var%==2 goto :jdk11
-if %var%==3 goto :jdk13
 if %var%==0 goto :salida
-if %var% GTR 3 goto :Error
+if %var% GTR 2 goto :Error
 goto :Menu
 
 :Error
 cls 
-echo ERROR: La opcion elegida no es valida. Debe indicar un valor entre 0 y 3
+echo ERROR: La opcion elegida no es valida. Debe indicar un valor entre 0 y 2
 echo Presiona una tecla para volver al menu
 pause > Nul
 goto :Menu
@@ -134,19 +136,6 @@ echo Presiona una tecla para volver al menu
 pause > Nul
 goto :Menu
 
-@rem Entorno de desarrollo JDK 13
-:jdk13
-cls 
-set jdkactivado=JDK 13
-echo Estableciendo JAVA_HOME 
-set JAVA_HOME=C:\des\bin\jdk13
-echo Estableciendo PATH 
-set PATHDES=%JAVA_HOME%\bin;%PATHDES%
-echo Version de java que se activara: %jdkactivado%
-echo Presiona una tecla para volver al menu
-pause > Nul
-goto :Menu
-
 @rem A la salida reiniciamos el PATH del sistema....
 :salida
 
@@ -166,16 +155,10 @@ set POWERSHELL=
 set PATHSYS=
 set PATHDES=
 set OPENSSL=
-set NODE=
 set GIT=
-set NPM=
 set PYTHON=
-set PERL=
-set MVN_HOME=
-set GRADLE_HOME=
-set GLASSFISH=
 set TOMCAT=
-set PAYARA=
+set TOMEE=
 set jdkactivado=
 set var=
 echo.
