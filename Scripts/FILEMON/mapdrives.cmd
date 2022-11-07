17a6861d75a7af039203acb60d018169c042f8e4
Juan F. Ruiz <juanfco.ruiz@outlook.es>
2022-11-07 13:40:55 +0100

    Elimino la carpeta FILEMON porque voy a regalar el portatil

--- Scripts/FILEMON/mapdrives.cmd
+++
@@ -1,2 +0,0 @@
-PowerShell -Command "Set-ExecutionPolicy -Scope CurrentUser Unrestricted" >> "%USERPROFILE%\StartupLog.txt" 2>&1 
-PowerShell -File "c:\des\bin\scripts\mapdrives.ps1" >> "%TEMP%\StartupLog.txt" 2>&1
