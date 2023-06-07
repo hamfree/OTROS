d74ba52943116dd9c461ecb6291437895a00b6c7
Juan F. Ruiz <h4mfr33@protonmail.com>
2023-06-07 12:49:01 +0200

    Elimino el directorio 'scripts' de este repositorio.

--- Scripts/ELSUPER/mapdrives.cmd
+++
@@ -1,2 +0,0 @@
-PowerShell -Command "Set-ExecutionPolicy -Scope CurrentUser Unrestricted" >> "%TEMP%\StartupLog.txt" 2>&1 
-PowerShell -File "d:\des\bin\scripts\mapdrives.ps1" >> "%TEMP%\StartupLog.txt" 2>&1
--- Scripts/logInit.cmd
+++
@@ -1,1 +0,0 @@
-@echo.Sesion iniciada en %date:~6,4%-%date:~3,2% - %date:~0,2% %time:~0,8% >> %USERPROFILE%\StartUpLog.txt
